be7a72c40c5e50235433f4300550873424ec730d
Gaboso <gabrielsantiago2@gmail.com>
2017-05-17 23:27:27 -0400

    Limpeza de itens faltantes do ultimo commit

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ------
 
-Esta é a página inicial, fique a vontade!!!
+Página inicial feita em AngularJS para listar os repositórios na minha conta.
--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,11 @@
-var app = angular.module('myApp', []);
-
-app.controller('homeCtrl', function ($scope, $http) {
+angular.module('myApp', [])
+.controller('homeCtrl', function ($scope, $http) {
 
     $http.get('https://api.github.com/users/gaboso')
         .then(function (response) {
             $scope.baseData =  response.data;
             $scope.$broadcast('LOAD_BASE_DATA');
         });
-
-    $scope.titulo = 'John';
 
     $scope.$on('LOAD_BASE_DATA', function () {
         $http.get('https://api.github.com/users/Gaboso/repos')
